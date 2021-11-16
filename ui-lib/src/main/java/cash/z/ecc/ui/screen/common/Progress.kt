342d6e2fbedb63effdf048d93b5d40a355421d21
Carter Jernigan <git@carterjernigan.com>
2021-11-16 15:57:59 -0500

    [#69] Android View for progress gradient

--- ui-lib/src/main/java/cash/z/ecc/ui/screen/common/Progress.kt
+++ ui-lib/src/main/java/cash/z/ecc/ui/screen/common/Progress.kt
@@ -1,8 +1,14 @@
 package cash.z.ecc.ui.screen.common
 
+import android.R
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.GradientDrawable
+import android.widget.ProgressBar
 import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.viewinterop.AndroidView
 import cash.z.ecc.ui.screen.onboarding.model.Progress
 import cash.z.ecc.ui.theme.ZcashTheme
 
@@ -14,4 +20,28 @@ fun PinkProgress(progress: Progress, modifier: Modifier = Modifier) {
         progress = progress.percent().decimal, modifier,
         ZcashTheme.colors.progressStart, ZcashTheme.colors.progressBackground
     )
+}
+
+@Composable
+fun AndroidProgressView(progress: Progress, modifier: Modifier = Modifier) {
+    val backgroundColor = ZcashTheme.colors.progressBackground.toArgb()
+    val startColor = ZcashTheme.colors.progressStart.toArgb()
+    val endColor = ZcashTheme.colors.progressEnd.toArgb()
+    AndroidView(
+        factory = {
+            ProgressBar(it, null, R.attr.progressBarStyleHorizontal).apply {
+                isIndeterminate = false
+                background = ColorDrawable(backgroundColor)
+                progressDrawable = GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,
+                    intArrayOf(startColor, endColor))
+
+                setProgress(progress.current.value)
+                max = progress.last.value
+            }
+        },
+        modifier = modifier,
+        update = {
+            it.progress = progress.current.value
+            it.max = progress.last.value
+        })
 }
